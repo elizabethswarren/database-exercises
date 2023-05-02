97f6a8a66586110ee07eb66680df99f7f2aff8e4
Elizabeth Warren <elizabeth.s.warren@gmail.com>
2023-05-02 11:27:00 -0500

    Add Basic Statement Exercises

---
+++ basic_statement_exercises.sql
@@ -0,0 +1,59 @@
+-- Basic Statements Exercises
+
+-- 1. 
+USE albums_db;
+
+-- 2.
+SHOW CREATE table albums;
+-- PRIMARY KEY (`id`)
+
+-- 3.
+SELECT *
+FROM albums;
+-- the name of the album
+
+-- 4. 
+	-- album sales
+    
+-- 5.
+SELECT *
+FROM albums
+WHERE artist = 'Pink Floyd';
+/* 'Pink Floyd','The Dark Side of the Moon'
+   'Pink Floyd','The Wall' */
+
+-- 6.
+SELECT *
+FROM albums
+WHERE artist = 'The Beatles';
+-- 1967
+
+-- 7.
+SELECT *
+FROM albums
+WHERE name = 'Nevermind';
+-- Grunge, alternative rock
+
+-- 8.
+SELECT *
+FROM albums
+WHERE release_date BETWEEN 1989 and 1999;
+-- 11
+
+-- 9. 
+SELECT name, sales AS low_selling_albums
+FROM albums
+WHERE sales < 20;
+/* 'Abbey Road','14.4'
+'Bad','19.3'
+'Born in the U.S.A.','19.6'
+'Brothers in Arms','17.7'
+'Dangerous','16.3'
+'Dirty Dancing','17.9'
+'Grease: The Original Soundtrack from the Motion Picture','14.4'
+'Let\'s Talk About Love','19.3'
+'Nevermind','16.7'
+'Sgt. Pepper\'s Lonely Hearts Club Band','13.1'
+'The Immaculate Collection','19.4'
+'The Wall','17.6'
+'Titanic: Music from the Motion Picture','18.1'*/
