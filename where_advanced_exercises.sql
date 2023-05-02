8d59aafd36263fbc434d76e9c292d907427851f5
Elizabeth Warren <elizabeth.s.warren@gmail.com>
2023-05-02 16:26:18 -0500

    Add instructor answers

--- where_advanced_exercises.sql
+++ where_advanced_exercises.sql
@@ -17,6 +17,12 @@ WHERE first_name = 'Irena'
 	OR 'Vidya'
     OR 'Maya';
 -- 10397, 10610, 10821. No, the numbers are not the same.
+/*Instructor Answer:
+SELECT *
+FROM employees
+WHERE first_name = 'Irena'
+	OR first_name = 'Vidya'
+    OR first_name = 'Maya';*/
 
 -- 3. 
 SELECT *
@@ -26,17 +32,33 @@ WHERE ( first_name = 'Irena'
     OR 'Maya')
     AND gender = 'M';
 -- 10397, 10821, 11327
+/* Instructor Answer:
+SELECT *
+FROM employees
+WHERE (first_name = 'Irena'
+	OR first_name = 'Vidya'
+    OR first_name = 'Maya')
+    AND gender = 'M'; */
 
 -- 4.
 SELECT DISTINCT *
 FROM employees
 WHERE last_name LIKE 'E%';
+/* Instructor Answer:
+SELECT DISTINCT last_name
+FROM employees
+WHERE last_name LIKE 'E%'; */
 
 -- 5.
 SELECT DISTINCT *
 FROM employees
 WHERE last_name LIKE 'E%'
 	OR last_name LIKE '%E';
+/* Instructor Answer:
+SELECT DISTINCT last_name
+FROM employees
+WHERE last_name LIKE 'E%'
+	OR last_name LIKE '%E';*/
     
 -- 6. 
 SELECT DISTINCT last_name
@@ -49,6 +71,10 @@ SELECT DISTINCT last_name
 FROM employees
 WHERE last_name LIKE '%E'
 	AND last_name LIKE 'E%';
+/* Instructor Answer:
+SELECT DISTINCT last_name
+FROM employees
+WHERE last_name LIKE 'E%E'; */
     
 -- 8.
 SHOW CREATE TABLE employees;
@@ -56,11 +82,17 @@ SHOW CREATE TABLE employees;
 SELECT *
 FROM employees
 WHERE hire_date BETWEEN '1989-12-31' AND '1999-12-31';
+/* Instructor Answer:
+SELECT *
+FROM employees
+WHERE hire_date LIKE '199%'; 
+10008, 10011, 10012 */
 
 -- 9.
 SELECT *
 FROM employees
 WHERE birth_date LIKE '%12-25';
+-- Instructor Answer: 10078, 10115, 10261
 
 -- 10.
 SELECT *
@@ -68,6 +100,12 @@ FROM employees
 WHERE (hire_date 
 	BETWEEN '1989-12-31' AND '1999-12-31')
     AND birth_date LIKE '%12-25';
+/* Instructor Answer: 
+SELECT *
+FROM employees
+WHERE hire_date LIKE '199%'
+	AND birth_date LIKE '%12-25'; 
+    10261, 10438, 10681*/
     
 -- 11.
 SELECT DISTINCT last_name
