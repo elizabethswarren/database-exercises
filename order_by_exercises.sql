1b1913f7ece0a4d7f9ccb95b75d40ac2f7265a6e
Elizabeth Warren <elizabeth.s.warren@gmail.com>
2023-05-03 10:22:23 -0500

    Add instructor answers

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -10,6 +10,11 @@ FROM employees
 WHERE first_name IN ('Irena', 'Vidya', 'Maya')
 ORDER BY first_name;
 -- Irena Reutenauer. Vidya Simmen
+/* Instructor Answer: For last name:
+SELECT first_name, last_name
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+ORDER BY first_name DESC; */
 
 -- 3.
 SELECT first_name, last_name
@@ -17,6 +22,11 @@ FROM employees
 WHERE first_name IN ('Irena', 'Vidya', 'Maya')
 ORDER BY first_name, last_name;
 -- Irena Acton. Vidya Zweizig
+/* Instructor Answer: For last name:
+SELECT first_name, last_name
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+ORDER BY first_name DESC, last_name DESC; */
 
 -- 4. 
 SELECT first_name, last_name
@@ -32,6 +42,12 @@ WHERE last_name LIKE 'E%'
 	AND last_name LIKE '%E'
 ORDER BY emp_no;
 -- 899 employees. 10021 Ramzi Erde.
+/* Instructor Answer:
+SELECT emp_no, first_name, last_name
+FROM employees
+WHERE last_name LIKE 'E%E'
+ORDER BY emp_no; 
+Tadhiro Erde for DESC. */
 
 -- 6.
 SELECT emp_no, first_name, last_name, hire_date
@@ -40,6 +56,7 @@ WHERE last_name LIKE 'E%'
 	AND last_name LIKE '%E'
 ORDER BY hire_date DESC;
 -- 899 employees. Vitaly Erie. Sergi Erde.
+-- Instructor Answer: Teiji Eldridge.
 
 -- 7.
 SELECT first_name, last_name, birth_date, hire_date
@@ -48,3 +65,4 @@ WHERE hire_date LIKE '199%'
 	AND birth_date LIKE '%-12-25'
 ORDER BY birth_date ASC, hire_date DESC;
 -- 362 employees. Pohua Sudkamp. Douadi Pettis.
+-- Instructor Answer: Khun Bernini.
