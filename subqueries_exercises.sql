1df3c895be9d1f48ccac084609d24cec39f8582f
Elizabeth Warren <elizabeth.s.warren@gmail.com>
2023-05-05 14:54:23 -0500

    Add subqueries exercises

---
+++ subqueries_exercises.sql
@@ -0,0 +1,66 @@
+-- Subqueries Exercises
+
+USE employees;
+
+-- 1.
+SELECT first_name, last_name
+FROM employees
+WHERE hire_date = (
+	SELECT hire_date
+    FROM employees
+    WHERE emp_no = 101010
+);
+
+-- 2.
+SELECT title
+FROM titles
+WHERE emp_no IN (
+	SELECT emp_no
+    FROM employees
+    WHERE first_name LIKE 'Aamod'
+)
+	AND to_date > NOW();
+
+-- 3.
+SELECT COUNT(*)
+FROM employees
+WHERE emp_no IN (
+	SELECT emp_no
+    FROM dept_emp
+    WHERE to_date < NOW()
+);
+-- 85108
+
+-- 4.
+SELECT first_name, last_name
+FROM employees
+WHERE emp_no IN (
+	SELECT emp_no
+    FROM dept_manager
+    WHERE to_date > NOW()
+)
+	AND gender = 'f';
+/* Isamu Legleitner
+   Karsten Sigstam
+   Leon DasSarma
+   Hilary Kambil */
+
+-- 5.
+SELECT COUNT(*)
+FROM salaries
+WHERE salary > (
+	SELECT AVG(salary)
+    FROM salaries
+)
+	AND to_date > NOW();
+
+-- 6.
+SELECT COUNT(emp_no)
+FROM salaries
+WHERE salary > 1 + (
+	SELECT STDDEV(salary)
+    FROM salaries
+    WHERE to_date > NOW()
+    ORDER BY salary DESC
+    LIMIT 1
+);
