1cba0949e96e7c5e7f44e27a5ef9cf801717f76b
Elizabeth Warren <elizabeth.s.warren@gmail.com>
2023-05-05 13:46:15 -0500

    Complete Join Exercises

--- join_exercises.sql
+++ join_exercises.sql
@@ -97,3 +97,61 @@ JOIN departments AS d
 WHERE de.to_date LIKE '9999-01-01'
 GROUP BY d.dept_no, d.dept_name
 ORDER BY d.dept_no;
+
+-- 7.
+SELECT d.dept_name,
+	AVG(s.salary) as average_salary
+FROM dept_emp AS de
+JOIN salaries AS s
+	ON s.emp_no = de.emp_no
+JOIN departments AS d
+	ON d.dept_no = de.dept_no
+WHERE s.to_date LIKE '9999-01-01'
+GROUP BY d.dept_name
+ORDER BY average_salary DESC
+LIMIT 1;
+
+-- 8.
+SELECT e.first_name, e.last_name,
+	MAX(s.salary) as highest_salary
+FROM dept_emp AS de
+JOIN salaries AS s
+	ON s.emp_no = de.emp_no
+JOIN departments AS d
+	ON d.dept_no = de.dept_no
+JOIN employees AS e
+	ON e.emp_no = de.emp_no
+WHERE d.dept_name LIKE 'marketing'
+	AND s.to_date LIKE '9999-01-01'
+GROUP BY e.first_name, e.last_name
+ORDER BY highest_salary DESC
+LIMIT 1;
+
+-- 9.
+SELECT e.first_name, e.last_name,
+	MAX(s.salary) as salary, d.dept_name
+FROM dept_emp AS de
+JOIN salaries AS s
+	ON s.emp_no = de.emp_no
+JOIN departments AS d
+	ON d.dept_no = de.dept_no
+JOIN employees AS e
+	ON e.emp_no = de.emp_no
+JOIN dept_manager AS dm
+	ON dm.emp_no = de.emp_no
+WHERE s.to_date LIKE '9999-01-01'
+	AND de.to_date LIKE '9999-01-01'
+GROUP BY e.first_name, e.last_name, d.dept_name
+ORDER BY salary DESC
+LIMIT 1;
+
+-- 10.
+SELECT d.dept_name,
+	ROUND(AVG(s.salary)) as average_salary
+FROM dept_emp AS de
+JOIN salaries AS s
+	ON s.emp_no = de.emp_no
+JOIN departments AS d
+	ON d.dept_no = de.dept_no
+GROUP BY d.dept_name
+ORDER BY average_salary DESC;
